17643fd469cc55f0047436ac56612bb0ef22ccef
hangilc <hangil@chang.jp>
2016-08-21 22:00:02 +0900

    incoporating drawer-svg

--- index.js
+++ index.js
@@ -7,13 +7,17 @@ var bodyParser = require("body-parser");
 var hogan = require("hogan");
 var fs = require("fs");
 var indexTmpl = hogan.compile(fs.readFileSync("./web-src/index.html", {encoding: "utf-8"}));
+var Shohousen = require("myclinic-drawer-forms").Shohousen;
 
 module.exports = function(config){
 	var app = express();
 	app.use(bodyParser.urlencoded({extended: false}));
 	app.use(bodyParser.json());
+	app.use(express.static("static"));
 	app.use("/", function(req, res){
-		var drawerPage = "var drawerPage = []";
+		var shohousen = new Shohousen();
+		var ops = shohousen.getOps();
+		var drawerPage = JSON.stringify([ops]);
 		res.end(indexTmpl.render({drawerPage: drawerPage}));
 	})
 	// app.use("/service", function(req, res){
@@ -60,7 +64,6 @@ module.exports = function(config){
 	// 		res.sendStatus(400);
 	// 	}
 	// });
-	app.use(express.static("static"));
 
 	return app;
 }
--- web-src/main.js
+++ web-src/main.js
@@ -1,2 +1,9 @@
 "use strict";
 
+var $ = require("jquery");
+var drawerToSvg = require("myclinic-drawer").drawerToSvg;
+
+var drawerPages = window.drawerPages;
+var ops = drawerPages[0];
+var dom = $("#preview-wrapper");
+dom.html("").append(drawerToSvg(ops, {width: "148mm", height: "210mm", viewBox: "0 0 148 210"}));
