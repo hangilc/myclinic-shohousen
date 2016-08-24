7376fbd7b8538ab75649767f6a7762a6ce6c2ed7
hangilc <hangil@chang.jp>
2016-08-24 19:49:27 +0900

    implemented svg output

--- web-src/main.js
+++ web-src/main.js
@@ -1,9 +1,10 @@
 "use strict";
 
-var $ = require("jquery");
-var drawerToSvg = require("myclinic-drawer").drawerToSvg;
+var drawerToSvg = require("myclinic-drawer-svg").drawerToSvg;
 
 var drawerPages = window.drawerPages;
 var ops = drawerPages[0];
-var dom = $("#preview-wrapper");
-dom.html("").append(drawerToSvg(ops, {width: "148mm", height: "210mm", viewBox: "0 0 148 210"}));
+var dom = document.getElementById("preview-wrapper");
+dom.appendChild(drawerToSvg(ops, {width: "148mm", height: "210mm", viewBox: "0 0 148 210"}));
+
+
